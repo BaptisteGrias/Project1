866d45fe80edc7466208f6b483669117955cea40
Sparco10 <julien-bourgineau_student2021@wilder.school>
2021-03-24 17:14:58 +0100

    update contact 3

---
+++ assets/notuse.js
@@ -0,0 +1,71 @@
+const wilders = document.querySelector(".photoWilders");
+wilders.style.position = "absolute";
+let position1 = 0;
+let direction = -1;
+
+function leftRight() {
+    if (position1 == 60) {direction = 1}
+    else if (position1 == -20) {direction = -1}
+
+    position1 += -2 * direction;
+    wilders.style.top = `${position1}px`;
+
+    requestAnimationFrame(leftRight);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* fiche contact */
+
+
+const myText = document.querySelectorAll(".input");
+
+myText.addEventListener('click', function (event){
+    myText.style.backgroundColor = "white";
+    myText.style.font-size = "14px";
+    myText.style.font-family = "Roboto";
+    console.log("event");
+});
+
+
+
+
+const userName = document.querySelector("#name");
+const userForm = document.querySelector("#submit-form");
+
+userForm.onsubmit = function(event) {
+  event.preventDefault();
+  alert(`Thank you  ${firstName.value}`);
+};
+
