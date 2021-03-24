ccf047d6d976865dbcd2690482c836c3c20b9edf
[violaine ernotte] <[e.violaine@yahoo.fr]>
2021-03-24 14:13:16 +0100

    avancée des animations JSdans fichier js + html + css

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,80 @@
-console.log("salut");
+/* scroll to top script-------------------------------------------------------------*/
 
 function scrollToTop() {
     window.scroll({top: 0, left: 0, behavior: 'smooth'});
 }
+
+
+/* dark mode-------------------------------------------------------------*/
+
+
+
+
+
+function functionDarkmode() {
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+  } 
+
+
+
+
+/* animation background-------------------------------------------------------------*/
+
+// Some random colors
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 20;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 90)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 50)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*scroll to top script de ? -------------------------------------------------------------*/
 
 scrollToTop();
 const wilders = document.querySelector(".photoWilders");
@@ -19,3 +91,5 @@ function leftRight() {
 
     requestAnimationFrame(leftRight);
 }
+
+/* end scroll to top script de ? -------------------------------------------------------------*/
