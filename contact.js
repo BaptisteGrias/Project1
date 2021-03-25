66c66aff67aaa98bce1805b2594f6a56434a7eaa
Sparco10 <julien-bourgineau_student2021@wilder.school>
2021-03-25 18:55:43 +0100

    version final

--- contact.js
+++ contact.js
@@ -1,11 +1,22 @@
 
-const myText = document.getElementById(".input-selector");
-console.log(myText)
-myText.addEventListener('click', function (event){
-    myText.style.backgroundColor = "white";
-    console.log(event);
-});
+const myText = document.getElementsByClassName("input-selector");
+for (let i=0 ; i < myText.length ; i++){
+  myText[i].addEventListener('click' , () => {
+    myText[i].style.backgroundColor = "white"; 
+    myText[i].style.fontSize = "16px"; 
+    myText[i].style.fontFamily = "Roboto"; 
+    myText[i].style.fontWeight = "bold";
+  })
+};
 
+
+const myMsg = document.getElementById("message-form");
+myMsg.addEventListener('click' , () => {
+    myMsg.style.backgroundColor = "white"; 
+    myMsg.style.fontFamily = "Roboto";
+    myMsg.style.fontSize = "16px";
+    myMsg.style.fontWeight = "bold";
+    })
 
 
 
@@ -15,6 +26,5 @@ const form = document.querySelector(".contact-form");
 
 form.onsubmit = function(event) {
   event.preventDefault();
-  alert(`Merci ${userName.value} de nous avoir contacter!`);
+  alert(`Merci ${userName.value} de nous avoir contacté!`);
 }
-
