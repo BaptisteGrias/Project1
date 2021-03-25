2acbe5c55e2c4380935fa78d04ac01b74434c8c6
Baptiste Grias <baptistegrias@MBP-de-Baptiste.home>
2021-03-25 18:50:49 +0100

    ajout fiche html de chaque wilder plus animation js de chaque wilder

--- fiche.js
+++ fiche.js
@@ -1,6 +1,6 @@
 //Text animation on the student name for the mobile version of the fiche pages
 
-const txtAnim = document.querySelector('h2');
+const txtAnim = document.querySelector('.baptiste');
 
 new Typewriter(txtAnim, {
     deleteSpeed: 10
@@ -9,11 +9,384 @@ new Typewriter(txtAnim, {
 .typeString("Bienvenue sur ma fiche !")
 .pauseFor(500)
 .deleteChars(24)
-.typeString("N'hésitez pas à me contactez ")
+.typeString("N'hésitez pas à me contacter ")
 .pauseFor(500)
 .deleteChars(29)
 .typeString("Bonne visite 🙂")
 .pauseFor(1000)
 .deleteChars(15)
 .typeString("Baptiste Grias")
-.start()
+.start();
+
+// animation aline
+const txtAnim1 = document.querySelector('.aline');
+
+new Typewriter(txtAnim1, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Aline Dubois")
+.start();
+
+
+// animation anais
+const txtAnim2 = document.querySelector('.anais');
+
+new Typewriter(txtAnim2, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Anais Berduchat")
+.start();
+
+
+
+// animation david
+const txtAnim3 = document.querySelector('.david');
+
+new Typewriter(txtAnim3, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("David D'Amato")
+.start();
+
+
+// animation edouard
+const txtAnim4 = document.querySelector('.edouard');
+
+new Typewriter(txtAnim4, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Edouard Larroche")
+.start();
+
+// animation gregory
+const txtAnim5 = document.querySelector('.gregory');
+
+new Typewriter(txtAnim5, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Gregory Bouteville")
+.start();
+
+
+// animation jerome
+const txtAnim6 = document.querySelector('.jerome');
+
+new Typewriter(txtAnim6, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Jérôme Shaaban")
+.start();
+
+
+// animation julienb
+const txtAnim7 = document.querySelector('.julienb');
+
+new Typewriter(txtAnim7, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Julien Bourgineau")
+.start();
+
+
+// animation julienN
+const txtAnim8 = document.querySelector('.julienN');
+
+new Typewriter(txtAnim8, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Julien Nicolas")
+.start();
+
+// animation kevin
+const txtAnim9 = document.querySelector('.kevin');
+
+new Typewriter(txtAnim9, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Kevin Touyard")
+.start();
+
+// animation manon
+const txtAnim10 = document.querySelector('.manon');
+
+new Typewriter(txtAnim10, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Manon Josse")
+.start();
+
+// animation marie-anne
+const txtAnim11 = document.querySelector('.marie');
+
+new Typewriter(txtAnim11, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Marie-Anne Duvieu")
+.start();
+
+// animation milad
+const txtAnim12 = document.querySelector('.milad');
+
+new Typewriter(txtAnim12, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Milad Behzadi")
+.start();
+
+
+// animation pauline
+const txtAnim13 = document.querySelector('.pauline');
+
+new Typewriter(txtAnim13, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Pauline Labbé")
+.start();
+
+
+// animation pierre
+const txtAnim14 = document.querySelector('.pierre');
+
+new Typewriter(txtAnim14, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Pierre Houot")
+.start();
+
+
+// animation sarah
+const txtAnim15 = document.querySelector('.sarah');
+
+new Typewriter(txtAnim15, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Sarah	Bensouna")
+.start();
+
+// animation sylvain
+const txtAnim16 = document.querySelector('.sylvain');
+
+new Typewriter(txtAnim16, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Sylvain Laconi")
+.start();
+
+
+// animation tom
+const txtAnim17 = document.querySelector('.tom');
+
+new Typewriter(txtAnim17, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Tom carpentier")
+.start();
+
+
+// animation tom
+const txtAnim18 = document.querySelector('.tom');
+
+new Typewriter(txtAnim18, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Tom carpentier")
+.start();
+
+// animation violaine
+const txtAnim19 = document.querySelector('.violaine');
+
+new Typewriter(txtAnim19, {
+    deleteSpeed: 10
+})
+.changeDelay(40)
+.typeString("Bienvenue sur ma fiche !")
+.pauseFor(500)
+.deleteChars(24)
+.typeString("N'hésitez pas à me contacter ")
+.pauseFor(500)
+.deleteChars(29)
+.typeString("Bonne visite 🙂")
+.pauseFor(1000)
+.deleteChars(15)
+.typeString("Violaine Ernotte")
+.start();
